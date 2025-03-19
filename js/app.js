3fc7ae4347b5ac58799b50a57a0437311e48d841
zinoo-dez <zinoo.dev@gmail.com>
2025-03-19 16:03:50 +0630

    fix code

--- js/app.js
+++ js/app.js
@@ -3,11 +3,15 @@ let rightLinksGroup = document.querySelector(".right-links-group");
 
 btn.addEventListener("click", () => {
      // alert('200')
-     // console.log(btn.src)
-     btn.src =  btn.src === "http://127.0.0.1:5500/img/svg/menu.svg" ? "http://127.0.0.1:5500/img/svg/cross.svg" : "http://127.0.0.1:5500/img/svg/menu.svg";
+     console.log(btn.src.includes("menu"))
+     if (btn.src.includes("menu")) {
+          btn.src = "./img/svg/cross.svg";
+     } else {
+          btn.src = "./img/svg/menu.svg";
+     }
      rightLinksGroup.classList.toggle("show");
 });
 window.addEventListener("scroll", () => {
      rightLinksGroup.classList.remove("show");
-     btn.src = "http://127.0.0.1:5500/img/svg/menu.svg";
+     btn.src = "./img/svg/menu.svg";
 });
